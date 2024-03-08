6627e40a627b1d07499e7517d1e04e32fbc9fcdb
riccifab <fabio.ricci@iit.it>
2024-03-08 17:20:10 +0100

    Bestemmie col PID

--- Teensy/src/Teensy_robot_client/main.cpp
+++ Teensy/src/Teensy_robot_client/main.cpp
@@ -40,6 +40,7 @@ PIDController motor2PID(2.5, 4.5, 1.2);
 PIDController motor3PID(2.2, 4.8, 1.1);
 
 void setMotorsSpeed(int vx, int vy, int w);
+void setMotorsSpeed_PID(int vx, int vy, int w);
 
     void setup()
 {
@@ -51,7 +52,7 @@ void setMotorsSpeed(int vx, int vy, int w);
 void loop()
 {
   static bool startFound = false;
-  static byte incomingBuffer[8];
+  static byte incomingBuffer[12];
   static int bufferIndex = 0;
 
   long int encoderCount1 = encoder.getEncoderCount(0); 
@@ -60,18 +61,6 @@ void loop()
   double currentSpeed1 = calculateSpeed(encoder.getEncoderCount(0));
   double currentSpeed2 = calculateSpeed(encoder.getEncoderCount(1));
   double currentSpeed3 = calculateSpeed(encoder.getEncoderCount(2));
-  // Print the counts to the serial monitor
-  delay(1000);
-  Serial.print("Encoder 1 Count: ");
-  Serial.println(encoderCount1);
-  Serial.print("Encoder 2 Count: ");
-  Serial.println(encoderCount2);
-  Serial.print("Encoder 3 Count: ");
-  Serial.println(encoderCount3);
-
-  double controlSignal1 = motor1PID.update(currentSpeed1);
-  double controlSignal2 = motor2PID.update(currentSpeed2);
-  double controlSignal3 = motor3PID.update(currentSpeed3);
   while (Serial1.available() > 0)
   {
     byte incomingByte = Serial1.read();
@@ -85,15 +74,16 @@ void loop()
     else if (startFound)
     {
       // Check if we've reached the end byte
-      if (incomingByte == 0x55 && bufferIndex == 8)
+      if (incomingByte == 0x55 && bufferIndex == 12)
       {
         // We've found a complete packet, process it
-        float vx, vy;
+        float vx, vy, w;
         memcpy(&vx, &incomingBuffer[0], sizeof(vx));
         memcpy(&vy, &incomingBuffer[4], sizeof(vy));
+        memcpy(&w, &incomingBuffer[8], sizeof(w));
 
         // Use the velocities as needed
-        setMotorsSpeed(vx * 250, vy * 250, 0);
+        setMotorsSpeed(vx * 250, vy * 250, w);
 
         Serial.print("Velocity X: ");
         Serial.print(vx);
@@ -103,7 +93,7 @@ void loop()
         startFound = false; // Reset for the next packet
         continue;           // Skip the rest of the loop
       }
-      else if (bufferIndex < 8)
+      else if (bufferIndex < 12)
       {
         // Collect data into buffer
         incomingBuffer[bufferIndex++] = incomingByte;
@@ -120,13 +110,18 @@ void loop()
 void setMotorsSpeed(int vx, int vy, int w)
 {
   std::array<int, 3> motorSpeeds = calculateMotorSpeeds(vx, vy, w);
-  
-    Serial.print("Motor 1 Speed: ");
-    Serial.println(motorSpeeds[0]);
-    Serial.print("Motor 2 Speed: ");
-    Serial.println(motorSpeeds[1]);
-    Serial.print("Motor 3 Speed: ");
-    Serial.println(motorSpeeds[2]);
+  motor1.drive(motorSpeeds[0]);
+  motor2.drive(motorSpeeds[1]);
+  motor3.drive(motorSpeeds[2]);
+
+}
+
+void setMotorsSpeed_PID(int vx, int vy, int w)
+{
+  std::array<int, 3> motorSpeeds = calculateMotorSpeeds(vx, vy, w);
+  double controlSignal1 = motor1PID.update(currentSpeed1);
+  double controlSignal2 = motor2PID.update(currentSpeed2);
+  double controlSignal3 = motor3PID.update(currentSpeed3);
   motor1.drive(motorSpeeds[0]);
   motor2.drive(motorSpeeds[1]);
   motor3.drive(motorSpeeds[2]);
